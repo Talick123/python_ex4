a5fd314ce3961aa707cb44b7b8316d9ddba56dab
Tali Kalev <talkalev@gmail.com>
2022-05-21 16:41:27 +0300

    syntax changes

--- q2.py
+++ q2.py
@@ -6,7 +6,8 @@ def question1():
         print("file log cannot be open")
     output_file = open("output.txt", 'w')
     txt = log.read()
-    messages = re.findall(r'[\w]+\s([0-9]+)[\w\s]+\.[A-Z\s]+([\w]+\.dat)\.\s[0-9]+\.[0-9]+[\w\s]+\.', txt)
+    #messages = re.findall(r'[\w]+\s([0-9]+)[\w\s]+\.[A-Z\s]+([\w]+\.dat)\.\s[0-9]+\.[0-9]+[\w\s]+\.', txt)
+    messages = re.findall(r'[\w]+\s([0-9]+)[A-Z\s\.]+([\w]+\.dat)\.\s[0-9]+\.[0-9]+[\w\s]+\.', txt)
     for message in messages:
         output_file.write(message[0] + " " + message[1] + "\n")
         #print(message[0] + " " + message[1])
